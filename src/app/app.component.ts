5426a9049e4f85e3fcd6b00a744ac971db76b5fc
Lucy Veron <lucymveron@gmail.com>
2021-02-11 15:35:06 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'conveyor-belt';
+
+  /* 
+    List-Detail View: Select a menu item from the LIST view and watch it appear in the DETAIL view.
+    Use the OnPush change detection strategy
+  */
 }
