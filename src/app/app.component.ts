8ef939dab396ec5c1960d4bdd2540374f21ca678
Lucy Veron <lucymveron@gmail.com>
2021-02-12 14:00:54 +0100

    List of menu items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'conveyor-belt';
-
+  
   /* 
     List-Detail View: Select a menu item from the LIST view and watch it appear in the DETAIL view.
     Use the OnPush change detection strategy
   */
+ 
+  public items = [
+    {name: 'sushi', emoji: '🍣'},
+    {name: 'soup', emoji: '🍜'},
+    {name: 'burger', emoji: '🍔'},
+    {name: 'pizza', emoji: '🍕'},
+    {name: 'ice cream', emoji: '🍨'},
+    {name: 'shrimp', emoji: '🍤'},
+    {name: 'chicken', emoji: '🍗'}
+  ]
 }
